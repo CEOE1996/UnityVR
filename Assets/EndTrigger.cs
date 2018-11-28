b2ad3d25484eeb13312a42afb7cfcaf48947bbd0
Carlos Orozco <ceoe1996@hotmail.com>
2018-11-27 23:46:50 -0600

    Final Project

---
+++ Assets/EndTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour {
+
+	void OnTriggerEnter()
+    {
+        CompleteLevel();
+    }
+
+    public void CompleteLevel()
+    {
+        Debug.Log("Finish!");
+        SceneManager.LoadScene("Finish");
+    }
+
+}
